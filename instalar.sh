5ba79af014d064c15cf6a57d7ede6ebaa885f9b6
pseudo-minimalista <difernandes@tutanota.com>
2022-12-24 10:29:18 -0300

    Configurações do XDG adicionadas

--- instalar.sh
+++ instalar.sh
@@ -5,7 +5,7 @@ doas apk add lightdm bash gnupg pass git make gcc g++ libx11-dev libxft-dev \
 libxinerama-dev libjpeg-turbo-dev libpng-dev giflib-dev libexif-dev imlib2-dev \
 libxrandr-dev linux-headers argp-standalone ncurses dbus-x11 xdg-utils xclip dunst \
 gcr-dev webkit2gtk-dev xprop alsa-utils vim udisks2 libgit2-dev xbacklight \
-xset xsetroot setxkbmap &&
+xset xsetroot setxkbmap zathura zathura-pdf-mupdf &&
 
 # Habilitando LightDM
 doas rc-update add lightdm
@@ -26,12 +26,15 @@ doas make clean install --directory=surf/		;
 
 # Copia de arquivos
 mkdir ~/.dwm 
+mkdir ~/.config
+cp -r .config/* ~/.config
 cp .autostart.sh ~/.dwm/autostart.sh
 cp .Xresources ~/
 doas cp .todo /usr/local/bin/todo
 doas cp .dfm /usr/local/bin/dfm
-doas cp dwm.desktop /usr/share/xsessions &&
-doas cp lightdm.conf /etc/lightdm/ &&
+doas cp dwm.desktop /usr/share/xsessions 
+doas cp zathura.desktop /usr/share/applications
+doas cp lightdm.conf /etc/lightdm/ 
 chmod +x ~/.dwm/autostart.sh
 
 # Reiniciar o computador
