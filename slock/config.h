f483f10e18579d6f6f30be4fc5a48df2ee15000a
pseudo-minimalista <difernandes@tutanota.com>
2022-12-24 10:18:21 -0300

    Primeiro commit

---
+++ slock/config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#11111b",     /* after initialization */
+	[INPUT] =  "#a6e3a1",   /* during input */
+	[FAILED] = "#f38ba8",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
