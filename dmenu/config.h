fe82788dc8bae907b104601e9a275591d8c5dbaf
pseudo-minimalista <pseudo-minimalista@noreply.codeberg.org>
2022-12-26 01:32:18 +0000

    Atualizar 'dmenu/config.h'

--- dmenu/config.h
+++ dmenu/config.h
@@ -8,9 +8,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cdd6f4", "#313244" },
-	[SchemeSel] = { "#cdd6f4", "#181825" },
-	[SchemeOut] = { "#313244", "#181825" },
+	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
+	[SchemeSel] = { "#1e1e2e", "#89dceb" },
+	[SchemeOut] = { "#000000", "#89dceb" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
