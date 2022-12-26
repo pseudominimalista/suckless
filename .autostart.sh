703e79ae8009fd8c2020963dd19a8f7745a94b13
pseudo-minimalista <pseudo-minimalista@noreply.codeberg.org>
2022-12-26 01:40:42 +0000

    Atualizar '.autostart.sh'

--- .autostart.sh
+++ .autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xsetroot -bg \#1e1e2e -fg \#1e1e2e -gray; bgs ~/.wp; xrdb -merge ~/.Xresources; xset led named "Scroll Lock"
+xsetroot -bg \#1e1e2e -fg \#1e1e2e -gray; bgs ~/.wp; xset led named "Scroll Lock"; setxkbmap br
 /usr/local/bin/dwmblocks 
