5bf373ca863a5c68f7353afccfe4f0fabcd6181e
pseudo-minimalista <difernandes@tutanota.com>
2023-01-03 00:23:46 -0300

    massive overhaul

--- st/config.h
+++ st/config.h
@@ -204,9 +204,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_equal,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_minus,       zoom,           {.f = -1} },
 	{ TERMMOD,              XK_0,        	zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ MODKEY,              	XK_c,           clipcopy,       {.i =  0} },
+	{ MODKEY,              	XK_v,           clippaste,      {.i =  0} },
+	{ MODKEY,              	XK_y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
